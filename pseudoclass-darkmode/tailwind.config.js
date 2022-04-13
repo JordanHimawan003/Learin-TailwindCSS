92991872adc1f1f8d5aa703ffa464549e6871606
JordanHimawan003 <jordan.hiphop21@gmail.com>
2022-04-13 18:49:57 +0700

    pseudo class and darkmode

---
+++ pseudoclass-darkmode/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+      colors: {
+        'permenkaret': '#ff77e9',
+      },
+    },
+  },
+  plugins: [],
+}
